dc8708f2884d8125be28b16ae15525fb2be9498c
Alex Tarasov <tarasof.av@gmail.com>
2024-04-15 17:56:54 +0300

    Initial commit

---
+++ src/main/java/org/example/shapes/Rectangle.java
@@ -0,0 +1,19 @@
+package org.example.shapes;
+
+public class Rectangle {
+    private final double width;
+    private final double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+}
---
+++ src/main/java/org/example/shapes/Triangle.java
@@ -0,0 +1,22 @@
+package org.example.shapes;
+
+public class Triangle {
+    private final double side1;
+    private final double side2;
+    private final double side3;
+
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public double getArea() {
+        double s = (side1 + side2 + side3) / 2;
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
+
+    public double getPerimeter() {
+        return side1 + side2 + side3;
+    }
+}
