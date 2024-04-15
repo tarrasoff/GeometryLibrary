5426e60472fdb9f3bcb8a277bf075995b7bf1197
Alex Tarasov <tarasof.av@gmail.com>
2024-04-15 17:58:34 +0300

    initial commit 2

--- GeometryUtils/src/main/java/org/example/utils/GeometryUtils.java
+++ GeometryUtils/src/main/java/org/example/utils/GeometryUtils.java
@@ -1,4 +1,22 @@
 package org.example.utils;
 
 public class GeometryUtils {
+    public static double convertToSquareMeters(double value, String unit) {
+        switch (unit.toLowerCase()) {
+            case "square centimeters":
+                return value / 10000;
+            case "square millimeters":
+                return value / 1000000;
+            default:
+                return value;
+        }
+    }
+
+    public static boolean compareAreas(double area1, double area2) {
+        return Double.compare(area1, area2) == 0;
+    }
+
+    public static boolean comparePerimeters(double perimeter1, double perimeter2) {
+        return Double.compare(perimeter1, perimeter2) == 0;
+    }
 }
